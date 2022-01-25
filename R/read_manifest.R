8d10e06cef5165b0401f71fb939424b1fa739765
Jeff Sullivan <jeff@entityrisk.com>
2022-01-25 17:12:29 -0500

    manifest metadata functions and tests for them

---
+++ R/read_manifest.R
@@ -0,0 +1,40 @@
+#' Return a list of the data files referenced by this manifest
+#'
+#' @param manifest path to a manifest file collocated with data
+#'
+#' @return vector of file names
+#' @export
+#'
+manifest_filelist <- function(manifest) {
+  j <- jsonlite::read_json(manifest)
+  local_path <- dirname(manifest)
+  filenames <- lapply(j$entries, function(e) basename(e$url))
+  file.path(local_path, filenames)
+}
+
+#' Extract the header names for the data
+#'
+#' @param manifest local path to the manifest file
+#'
+#' @return vector of header names, in the order in which they appear; vector of
+#' column types, in the same order
+#' @export
+#'
+#' @examples
+manifest_headers <- function(manifest) {
+  list(names = c(),
+       types = c()
+  )
+}
+
+#' Extract the total number of records according to the manifest
+#'
+#' @param manifest local path to the manifest file
+#'
+#' @return integer of the total number of records in the manifest
+#' @export
+#'
+#' @examples
+manifest_records <- function(manifest) {
+  -1
+}
---
+++ tests/testthat/testread_manifest.R
@@ -0,0 +1,18 @@
+test_that("Extracted filenames are correct", {
+          expect_equal(
+            basename(manifest_filelist(system.file("extdata", "venuemanifest", package = "ReadRedshift"))),
+            c("venue_csv0000", "venue_csv0001")
+          )
+})
+
+test_that("Extracted header names are correct", {
+  expect_equal(manifest_headers(system.file("extdata", "venuemanifest", package = "ReadRedshift"))$names,
+               names(venue)
+  )
+})
+
+test_that("Manifest number of records is correct", {
+  expect_equal(manifest_records(system.file("extdata", "venuemanifest", package = "ReadRedshift")),
+               nrow(venue)
+  )
+})
