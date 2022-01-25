46ec2c44c24d97666c37256ba7f5b1ab1fa7b950
Jeff Sullivan <jeff@entityrisk.com>
2022-01-25 13:34:14 -0500

    fix typos

--- R/readRedshift.R
+++ R/readRedshift.R
@@ -1,6 +1,6 @@
 #' readRedshift will read in the contents of a manifest and return a data.frame
 #'
-#' @param manifest_file path to a RedShift manifest file
+#' @param manifest_file path to a Redshift manifest file
 #'
 #' @return the data.frame containing the validated data specified
 #' by the manifest file. Delimiters will be detected automatically unless one
