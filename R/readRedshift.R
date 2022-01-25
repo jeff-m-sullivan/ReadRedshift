31fb42f7a4887c3fab61f2311c5264afb9101fe8
Jeff Sullivan <jeff@entityrisk.com>
2022-01-25 13:27:48 -0500

    stub function that doesn't work

---
+++ R/readRedshift.R
@@ -0,0 +1,13 @@
+#' readRedshift will read in the contents of a manifest and return a data.frame
+#'
+#' @param manifest_file path to a RedShift manifest file
+#'
+#' @return the data.frame containing the validated data specified
+#' by the manifest file. Delimiters will be detected automatically unless one
+#' is specified.
+#' @export
+#'
+#' @examples
+readRedshift <- function(manifest_file) {
+  data.frame()
+}
