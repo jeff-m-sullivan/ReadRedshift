a8b7e449209242f7310f2555de225bfc524440fc
Jeff Sullivan <jeff@entityrisk.com>
2022-01-25 20:39:55 -0500

    apply styler

--- tests/testthat/testread_redshift.R
+++ tests/testthat/testread_redshift.R
@@ -1,20 +1,17 @@
-test_that(
-  "Genereated data dimensions match ground truth", {
+test_that("Genereated data dimensions match ground truth", {
   expect_equal(nrow(read_redshift(
-                      system.file("extdata", "venuemanifest", package = "ReadRedshift")
-                    )),
-               nrow(venue))
+    system.file("extdata", "venuemanifest", package = "ReadRedshift")
+  )),
+  nrow(venue))
   expect_equal(ncol(read_redshift(
-                      system.file("extdata", "venuemanifest", package = "ReadRedshift")
-                    )),
-               ncol(venue))
+    system.file("extdata", "venuemanifest", package = "ReadRedshift")
+  )),
+  ncol(venue))
 })
 
 test_that("Generated data has proper names", {
-  expect_equal(names(
-                 read_redshift(
-                   system.file("extdata", "venuemanifest", package = "ReadRedshift")
-                 )
-               ),
-               names(venue))
+  expect_equal(names(read_redshift(
+    system.file("extdata", "venuemanifest", package = "ReadRedshift")
+  )),
+  names(venue))
 })
