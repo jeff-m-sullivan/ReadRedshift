53663ee8812f59a4a9b6907b5e56dba31e1b2b2d
Jeff Sullivan <jeff@entityrisk.com>
2022-01-31 12:31:24 -0500

    spitting out SQL files

--- R/read_redshift.R
+++ R/read_redshift.R
@@ -37,7 +37,7 @@ read_redshift <- function(manifest_file) {
 insert_header <- function(manifest_file) {
   files <- manifest_filelist(manifest_file)
   headers <- manifest_headers(manifest_file)$names
-  header_row <- paste(headers, collapse=",")
+  header_row <- paste(headers, collapse =",")
 
   fout <- file(paste0(manifest_file, "_edit.sh"))
   writeLines(paste0("sed -i '1s/^/",
@@ -117,6 +117,16 @@ redshift_to_sql_data <- function(type_list) {
 #'
 #' @examples
 generate_sql_load <- function(manifest_file, s3_url) {
+  outfile <- file(paste0(manifest_file, ".sql"), open="wt")
+  table_name <- sub("*manifest$", "", basename(manifest_file))
+  writeLines(c(paste0("create table if not exists ", table_name, " (")), outfile)
 
-
+  variables <- jsonlite::read_json(manifest_file)$schema$elements
+  varstmts <- sapply(variables, FUN=function(e) paste0(e$name, "\t", redshift_to_sql_data(e$type), ","))
+  writeLines(varstmts, outfile)
+  writeLines(c(");","",
+               paste0("copy ", table_name),
+               paste0("from '", s3_url, "'/", basename(manifest_file)),
+               "manifest"), outfile)
+  close(outfile)
 }
