f81a5d7391eb7cda02f5a5e207ac0f252fbba148
Jeff Sullivan <jeff@entityrisk.com>
2022-01-31 13:23:01 -0500

    fix path typo

--- R/read_redshift.R
+++ R/read_redshift.R
@@ -136,7 +136,7 @@ generate_sql_load <- function(manifest_file, s3_url) {
   writeLines(c(
     ");", "",
     paste0("copy ", table_name),
-    paste0("from '", s3_url, "'/", basename(manifest_file)),
+    paste0("from '", s3_url, "/", basename(manifest_file),"'"),
     "manifest"
   ), outfile)
   close(outfile)
